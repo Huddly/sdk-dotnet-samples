ad1a118ec7c3fe18fbe97ab9d0ac66aed993e130
Eskil Høstad <eskil.hostad@huddly.com>
2023-08-03 16:36:47 +0200

    sample for consuming detections

--- Detections/Program.cs
+++ Detections/Program.cs
@@ -1,27 +1,31 @@
-﻿using Huddly.CameraProto;
-using Huddly.Sdk;
+﻿using Huddly.Sdk;
 using Huddly.Sdk.Detectors;
-using Huddly.Sdk.Devices;
 using Huddly.Sdk.Models;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging.Abstractions;
 
 namespace Detections;
 
 internal class Program
 {
-    static int numDevicesConnected = 0;
     static async Task Main(string[] args)
     {
-        ISet<IDeviceMonitor> monitors = Huddly.Sdk.Monitor.DefaultFor(new NullLoggerFactory(), ConnectionType.IP);
+        ISet<IDeviceMonitor> monitors = Huddly.Sdk.Monitor.DefaultFor(
+            new NullLoggerFactory(),
+            ConnectionType.IP
+        );
 
         // Should always be disposed after use
         ISdk huddlySdk = Sdk.Create(new NullLoggerFactory(), monitors);
 
         var cts = new CancellationTokenSource();
+
+        // Create a separate cts specifically for cancelling detectors.
         var detectorCts = CancellationTokenSource.CreateLinkedTokenSource(cts.Token);
-        var signal = new SemaphoreSlim(0, 1);
-        huddlySdk.DeviceConnected += (sender, eventArgs) => HandleDeviceConnected(sender, eventArgs, detectorCts.Token, signal);
+        // For signalling when detectors have been disposed properly
+        var signal = new SemaphoreSlim(1, 1);
+
+        huddlySdk.DeviceConnected += (sender, eventArgs) =>
+            HandleDeviceConnected(sender, eventArgs, detectorCts.Token, signal);
 
         Console.WriteLine("Press Control+C to quit the sample.");
         Console.CancelKeyPress += async (sender, eventArgs) =>
@@ -29,9 +33,11 @@ internal class Program
             Console.WriteLine("Cancellation requested; will exit.");
             eventArgs.Cancel = true;
 
-            Console.WriteLine("Tearing down detector");
+            // First cancel all running detectors
             detectorCts.Cancel();
+            // Wait for the running detectors to be stopped and disposed properly
             await signal.WaitAsync();
+            // Only after the detectors have been disposed do we cancel/dispose the sdk.
             cts.Cancel();
         };
         Task sdkTask = huddlySdk.StartMonitoring(ct: cts.Token);
@@ -39,14 +45,28 @@ internal class Program
         huddlySdk.Dispose();
     }
 
-    private static async void HandleDeviceConnected(object? sender, DeviceConnectionChangeEventArgs eventArgs, CancellationToken ct, SemaphoreSlim signal)
+    private static async void HandleDeviceConnected(
+        object? sender,
+        DeviceConnectionChangeEventArgs eventArgs,
+        CancellationToken ct,
+        SemaphoreSlim signal
+    )
     {
-        if (numDevicesConnected++ > 0)
-            return;
         IDevice device = eventArgs.Device;
         Console.WriteLine($"Device {device} connected");
 
-        var detectorOptions = new DetectorOptions(DetectorMode.AlwaysOn, DetectionConvertion.Relative);
+        await signal.WaitAsync();
+        await ConsumeDetections(device, ct);
+        // Release the signal to indicate that the detector has been disposed gracefully.
+        signal.Release();
+    }
+
+    private static async Task ConsumeDetections(IDevice device, CancellationToken ct)
+    {
+        var detectorOptions = new DetectorOptions(
+            DetectorMode.AlwaysOn,
+            DetectionConvertion.Relative
+        );
         Result<IDetector> detectorResult = await device.GetDetector(detectorOptions, ct);
         if (!detectorResult.IsSuccess)
         {
@@ -55,14 +75,18 @@ internal class Program
         }
 
         IDetector detector = detectorResult.Value;
-        while (!ct.IsCancellationRequested)
+        // This loop will continue indefinitely until either:
+        // 1. The token passed to GetDetections is cancelled.
+        // 2. The detector is disposed.
+        // Note that the IAsyncEnumerable returned by GetDetections does not throw in either of these cases.
+        await foreach (Huddly.Sdk.Models.Detections detections in detector.GetDetections(ct))
         {
-            await foreach (Huddly.Sdk.Models.Detections detections in detector.GetDetections(ct))
-            {
-                Console.WriteLine(detections.Count());
-            }
+            int personBoxCount = detections.Count(detection => detection.Label == "person");
+            int headBoxCount = detections.Count(detection => detection.Label == "head");
+            Console.WriteLine($"Received detections with {personBoxCount} person boxes and {headBoxCount} head boxes");
         }
-
-        signal.Release();
+        // Always dispose the detector properly after use.
+        // This should be completed before disposing/cancelling the ISdk from which the detector has been derived.
+        await detector.DisposeAsync();
     }
 }
