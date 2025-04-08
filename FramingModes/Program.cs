39fa6e3445781863f9830de59105e14c78e7c32d
Thomas Mittet <thomas@huddly.com>
2025-04-08 09:10:31 +0200

    Use correct namespace for FramingModes sample project

--- FramingModes/Program.cs
+++ FramingModes/Program.cs
@@ -3,7 +3,7 @@ using Huddly.Sdk.Models;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace CameraInfo;
+namespace FramingModes;
 
 internal class Program
 {
